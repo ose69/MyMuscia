bf22d9c3ce4edcf70082ad1ddeafb5eb941d0b49
5j <5j@iMac-de-ANDREA.local>
2019-11-06 08:32:51 -0600

    first commit

---
+++ App.js
@@ -0,0 +1,25 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import LoginView from './src/LoginView';
+import HomeView from './src/HomeView';
+import {Actions, Scene, Router} from 'react-native-router-flux';
+
+const scenes =Actions.create(
+  <Scene key='root'>
+  <Scene key='login' component={LoginView} hideNavBar></Scene>
+  <Scene key='home' component={HomeView} hideNavBar></Scene>
+  </Scene>
+);
+
+export default class App extends Component<Props>{
+  render(){
+    return<Router scenes={scenes}></Router>
+  }
+}
